d86cde1895f4f1026e3092a105615dd3ddc1cf16
Littlefield, Tyler [BWIUS NON-J&J] <tlittlef@its.jnj.com>
2022-02-19 16:51:45 -0800

    try again

--- r_deps.R
+++ r_deps.R
@@ -1,3 +1,4 @@
+options(repos = c(CRAN = 'https://packagemanager.rstudio.com/all/__linux__/focal/latest'))
 install.packages(c(
   "tidymodels",
   "textrecipes",
---
+++ sys_deps.sh
@@ -0,0 +1,7 @@
+apt-get update -y
+apt-get install -y libssl-dev libcurl4-openssl-dev libxml2-dev
+apt-get install -y libicu-dev
+apt-get install -y libglpk-dev
+apt-get install -y libgmp3-dev
+apt-get install -y libxml2-dev
+apt-get install -y pandoc
